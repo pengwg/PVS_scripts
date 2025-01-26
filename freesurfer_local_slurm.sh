581012fb52e9bf6c183c09a43ddc802f4c0221ca
Peng Wang <pengwg@gmail.com>
2025-01-26 12:23:49 -0500

    Re-run freesurfer with T2

--- freesurfer_local_slurm.sh
+++ freesurfer_local_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=0-29  
+#SBATCH --array=91-140%30  
 #SBATCH --partition=defq
 #SBATCH --ntasks=1                   
 #SBATCH --cpus-per-task=10            
@@ -9,6 +9,14 @@ FILE_PATH=/home/pw0032/PVS_nii
 
 mapfile -t FILES_NUM < file_numbers.txt
 
-FILE_TO_PROCESS=PVS_${FILES_NUM[$SLURM_ARRAY_TASK_ID]}_T1_RAGE_SAG.nii.gz
+TASK_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
+# TASK_ID=${FILES_NUM[$SLURM_ARRAY_TASK_ID]}
 
-srun recon-all -all -hires -parallel -openmp 10 -i "$FILE_PATH/$FILE_TO_PROCESS" -s PVS_${FILES_NUM[$SLURM_ARRAY_TASK_ID]}
+T1_VOL=PVS_${TASK_ID}_T1_RAGE_SAG.nii.gz
+T2_VOL=PVS_${TASK_ID}_T2_SPACE_AX.nii.gz
+FLAIR_VOL=PVS_${TASK_ID}_T2_SPACE_FLAIR_AX.nii.gz
+
+# srun recon-all -all -hires -parallel -openmp 10 -i "$FILE_PATH/$T1_VOL" -s PVS_$TASK_ID
+
+# Rerun with T2
+srun recon-all -all -hires -parallel -openmp 10 -T2 "$FILE_PATH/$T2_VOL" -s PVS_$TASK_ID
