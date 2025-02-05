7d85419fde28810ee78a7d1df30008d6c0d7f0db
Peng Wang <pengwg@gmail.com>
2025-02-05 16:40:13 -0500

    LST script improvement

--- lst_slurm.sh
+++ lst_slurm.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-#SBATCH --array=1-140%5  
+#SBATCH --array=1-140
 #SBATCH --ntasks=1                   
-#SBATCH --cpus-per-task=30            
+#SBATCH --cpus-per-task=40            
 #SBATCH --partition=gpu
 #SBATCH --gpus=2
 
-FILE_PATH=/home/pw0032/PVS_nii
+DATA_PATH=/home/pw0032/PVS_nii
 
-TASK_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
+# mapfile -t FILES_NUM < lst_work_number.txt
+# FILE_ID=$(echo "${FILES_NUM[$SLURM_ARRAY_TASK_ID]}" | xargs)
+FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 
-T1_VOL=PVS_${TASK_ID}_T1_RAGE_SAG.nii.gz
-T2_VOL=PVS_${TASK_ID}_T2_SPACE_AX.nii.gz
-FLAIR_VOL=PVS_${TASK_ID}_T2_SPACE_FLAIR_AX.nii.gz
+T1_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T1_RAGE_*.nii.gz" | head -n 1)
+FLAIR_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T2_SPACE_FLAIR*.nii.gz" | head -n 1)
+T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
 
 source /home/pw0032/lst-ai/lst/bin/activate
 
-srun lst --t1 $FILE_PATH/$T1_VOL --flair $FILE_PATH/$FLAIR_VOL --output /projects/2024-11_Perivascular_Space/LST/${TASK_ID} --temp /projects/2024-11_Perivascular_Space/LST/temp
+echo "Processing PVS_$FILE_ID"
+srun lst --t1 $T1_VOL --flair $FLAIR_VOL --output /projects/2024-11_Perivascular_Space/LST/${FILE_ID} --temp /projects/2024-11_Perivascular_Space/LST/temp-$FILE_ID
