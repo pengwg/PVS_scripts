1bd5b1bd50802dfec5abd68c4ab2ccb8a7cbe3fd
Peng Wang <pengwg@gmail.com>
2025-04-14 13:18:06 -0400

    Rename the freesurfer script

--- freesurfer_slurm.sh
+++ freesurfer_slurm.sh
@@ -7,10 +7,7 @@
 
 DATA_PATH=/projects/2024-11_Perivascular_Space/PVS_Data/PVS_nii
 
-mapfile -t FILES_NUM < /home/pw0032/scripts/file_numbers.txt
-
-# FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
-FILE_ID=$(echo "${FILES_NUM[$SLURM_ARRAY_TASK_ID]}" | xargs)
+FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 
 T1_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T1_RAGE_*.nii.gz" | head -n 1)
 T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
