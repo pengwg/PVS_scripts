889d2d74ad3344f9621bb993925ad6f95369feee
Peng Wang <pengwg@gmail.com>
2025-05-17 19:26:12 -0400

    Update frangi filter script

--- PVS_subjects_all.m
+++ PVS_subjects_all.m
@@ -1,13 +1,26 @@
+clear
+
+p = gcp('nocreate');
+if isempty(p)
+    parpool(12);
+end
+
+parfor n = 33 : 33
+    PVS_subject(n)
+end
+
+return
+
 function PVS_subject(id)
 
 addpath('filters/frangi_filter_version2a/')
 
-data_path = '/home/pw0032/FS';
+data_path = '/projects/2024-11_Perivascular_Space/batch1_output/FS';
 out_path = '/projects/2024-11_Perivascular_Space/batch1_output/PVS_vessel';
 
 Options.BlackWhite = false;
-Options.FrangiScaleRange = [0.1 4];
-Options.FrangiScaleRatio = 0.3;
+Options.FrangiScaleRange = [0.5 4];
+Options.FrangiScaleRatio = 0.5;
 Options.FrangiC = 60;
 
 
@@ -34,26 +47,26 @@ disp(['Measure  ' subject '...' ])
 info = niftiinfo(T2_nii);
 T2_vol = niftiread(info);
 
-% [J,Scale,Vx,Vy,Vz] = FrangiFilter3D(T2_vol, Options);
+[J,Scale,Vx,Vy,Vz] = FrangiFilter3D(T2_vol, Options);
 
-% niftiwrite(J, [out_path '/' subject '_vesselnessC60'], info, 'Compressed',true)
+niftiwrite(J, [out_path '/' subject '_vesselness'], info, 'Compressed',true)
 % niftiwrite(Scale, [out_path '/' subject '_ScaleC60'], info, 'Compressed',true)
-
-J = niftiread([out_path '/' subject '_vesselnessC60.nii.gz']);
-% Scale = niftiread([out_path '/' subject '_ScaleC60.nii.gz']);
+% J = niftiread([out_path '/' subject '_vesselness.nii.gz']);
 vessels = J;
 
-threshold = 20e-4;
+seg_vol = niftiread(seg_nii);
+ventricals = ismember(seg_vol, [4, 5, 14, 43, 44]);
+ventricals = imdilate(ventricals, strel('sphere', 5));
+hippocampus = ismember(seg_vol, [17, 53]);
+
+wmbg_mask = ismember(seg_vol, [2, 41, 11, 12, 13, 26, 50, 51, 52, 58]);
+wmbg_mask(ventricals | hippocampus) = 0;
+
+vessels(~wmbg_mask) = 0;
+threshold = 5e-3;
 vessels(vessels < threshold) = 0;
 vessels(vessels >= threshold) = 1;
 
-seg_vol = niftiread(seg_nii);
-wm_mask = (seg_vol == 2) | (seg_vol == 41);
-wm_mask = imerode(wm_mask, strel('sphere',3));
-bg_mask = ismember(seg_vol, [11, 12, 13, 26, 50, 51, 52, 58]);
+niftiwrite(vessels, [out_path '/' subject '_PVS_wmbg'], info, 'Compressed',true)
 
-vessels(~(wm_mask | bg_mask)) = 0;
-
-niftiwrite(vessels, [out_path '/' subject '_vesselmask'], info, 'Compressed',true)
-
-return
+end
