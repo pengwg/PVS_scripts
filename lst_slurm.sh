81d2e1840a66f3f232a33cf8aada4612b83ec831
Peng Wang <pengwg@gmail.com>
2025-05-22 14:03:16 -0400

    Update LST script to register flair to resampled T1 first

--- lst_slurm.sh
+++ lst_slurm.sh
@@ -2,21 +2,30 @@
 
 #SBATCH --array=1-140
 #SBATCH --ntasks=1                   
-#SBATCH --cpus-per-task=40            
+#SBATCH --cpus-per-task=20            
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 
 DATA_PATH=/home/pw0032/PVS_nii
+FS_PATH=/home/pw0032/Data/batch1_output/FS
 
-# mapfile -t FILES_NUM < lst_work_number.txt
-# FILE_ID=$(echo "${FILES_NUM[$SLURM_ARRAY_TASK_ID]}" | xargs)
 FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
 
-T1_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T1_RAGE_*.nii.gz" | head -n 1)
+T1_VOL=$(find $FS_PATH/PVS_$FILE_ID/mri/ -type f -name nu.mgz | head -n 1)
 FLAIR_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T2_SPACE_FLAIR*.nii.gz" | head -n 1)
 T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
 
+OUT_PATH=/home/pw0032/Data/batch1_output/LST/PVS_$FILE_ID
+
+if ! [ -d $OUT_PATH ]; then
+    mkdir $OUT_PATH
+fi
+
+mri_convert $T1_VOL $T1_VOL.nii.gz
+
+singularity exec -e /cm/shared/containers/ANTs.sif antsRegistrationSyNQuick.sh -d 3 -t r -f $T1_VOL.nii.gz -m $FLAIR_VOL -o $OUT_PATH/flair2T1_ -n 20
+
 source /home/pw0032/lst-ai/lst/bin/activate
 
-echo "Processing PVS_$FILE_ID"
-srun lst --t1 $T1_VOL --flair $FLAIR_VOL --output /projects/2024-11_Perivascular_Space/LST/${FILE_ID} --temp /projects/2024-11_Perivascular_Space/LST/temp-$FILE_ID
+echo "LST processing $OUT_PATH/fair2T1_Warped.nii.gz"
+srun lst --t1 $T1_VOL.nii.gz --flair $OUT_PATH/flair2T1_Warped.nii.gz --output $OUT_PATH/ --temp $OUT_PATH/tmp/
