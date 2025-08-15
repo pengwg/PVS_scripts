2cceb8bef8f1ef0207a497353060bfc890204821
Peng Wang <pengwg@gmail.com>
2025-08-15 03:42:57 -0400

    Modify freesurfer script to batch 4 data

--- freesurfer_slurm.sh
+++ freesurfer_slurm.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#SBATCH --array=0-8  
+#SBATCH --array=1-45%8
 #SBATCH --partition=defq
 #SBATCH --ntasks=1                   
 #SBATCH --cpus-per-task=20       
 
-DATA_PATH=/projects/2024-11_Perivascular_Space/PVS_Data/PVS_nii
+DATA_PATH=/projects/2024-11_Perivascular_Space/PVS_Data4
+export SUBJECTS_DIR=/projects/2024-11_Perivascular_Space/PVS_B4_Analysis/FS
 
-FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
+FILE_ID=$(printf "PVS_4_%03d" $SLURM_ARRAY_TASK_ID)
 
-T1_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T1_RAGE_*.nii.gz" | head -n 1)
-T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
+T1_VOL=$(find $DATA_PATH/$FILE_ID/ -type f -name "${FILE_ID}_T1_RAGE_*.nii" | head -n 1)
+T2_VOL=$(find $DATA_PATH/$FILE_ID/ -type f \( -name "${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
 
-echo "recon-all -all -hires -parallel -cw256 -openmp 20 -i $T1_VOL -T2 $T2_VOL -s PVS_$FILE_ID"
-srun recon-all -all -hires -parallel -cw256 -openmp 20 -i $T1_VOL -T2 $T2_VOL -s PVS_$FILE_ID
+echo "recon-all -all -hires -parallel -cw256 -openmp 20 -i $T1_VOL -T2 $T2_VOL -s $FILE_ID"
+srun recon-all -all -hires -parallel -cw256 -openmp 20 -i $T1_VOL -T2 $T2_VOL -s $FILE_ID
 
 # Rerun with T2
 # srun recon-all -all -hires -parallel -cw256 -openmp 20 -T2 $T2_VOL -s PVS_$FILE_ID
