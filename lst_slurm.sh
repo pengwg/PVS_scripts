3dec3ee92d4957e22f4ba86004d7ae3248770990
Peng Wang <pengwg@gmail.com>
2025-08-24 23:24:11 -0400

    Modify lst slurm for batch4 data

--- lst_slurm.sh
+++ lst_slurm.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#SBATCH --array=1-140
+#SBATCH --array=1-45
 #SBATCH --ntasks=1                   
 #SBATCH --cpus-per-task=20            
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
 
-DATA_PATH=/home/pw0032/PVS_nii
-FS_PATH=/home/pw0032/Data/batch1_output/FS
+DATA_PATH=/projects/2024-11_Perivascular_Space/PVS_Data4
+FS_PATH=/projects/2024-11_Perivascular_Space/PVS_B4_Analysis/FS
 
-FILE_ID=$(printf "%03d" $SLURM_ARRAY_TASK_ID)
+FILE_ID=$(printf "PVS_4_%03d" $SLURM_ARRAY_TASK_ID)
 
-T1_VOL=$(find $FS_PATH/PVS_$FILE_ID/mri/ -type f -name nu.mgz | head -n 1)
-FLAIR_VOL=$(find $DATA_PATH/ -type f -name "PVS_${FILE_ID}_T2_SPACE_FLAIR*.nii.gz" | head -n 1)
-T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
+T1_VOL=$(find $FS_PATH/$FILE_ID/mri/ -type f -name nu.mgz | head -n 1)
+FLAIR_VOL=$(find $DATA_PATH/$FILE_ID -type f -name "${FILE_ID}_T2_SPACE_FLAIR*.nii" | head -n 1)
+T2_VOL=$(find $DATA_PATH/ -type f \( -name "${FILE_ID}_T2_SPACE_AX*" -o -name "${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
 
-OUT_PATH=/home/pw0032/Data/batch1_output/LST/PVS_$FILE_ID
+OUT_PATH=/projects/2024-11_Perivascular_Space/PVS_B4_Analysis/LST/$FILE_ID
 
 if ! [ -d $OUT_PATH ]; then
     mkdir $OUT_PATH
