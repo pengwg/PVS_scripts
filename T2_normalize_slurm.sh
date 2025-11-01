056258de975cba503143718a6ddcfcf76716caba
Peng Wang <pengwg@gmail.com>
2025-11-01 03:42:54 -0400

    Create aseg and cso mask in T2 space

--- T2_normalize_slurm.sh
+++ T2_normalize_slurm.sh
@@ -20,11 +20,18 @@ fi
 
 T2_VOL=$(find $DATA_PATH/ -type f \( -name "PVS_${FILE_ID}_T2_SPACE_AX*" -o -name "PVS_${FILE_ID}_T2_SPACE_SAG*" \) | head -n 1)
 mri_convert $FS_MRI_DIR/aseg.mgz $OUTPUT_DIR/aseg.nii.gz
-singularity exec -e /cm/shared/containers/ANTs.sif ResampleImageBySpacing 3 $OUTPUT_DIR/aseg.nii.gz $OUTPUT_DIR/aseg.nii.gz 0.4 0.4 0.4 0 0 1
-mri_vol2vol --lta $FS_MRI_DIR/transforms/T2raw.lta --mov $T2_VOL --targ $OUTPUT_DIR/aseg.nii.gz --o $OUTPUT_DIR/T2_iso.nii.gz
 
-# Create CSO mask in native space
+# Resample aseg to 0.4 isotropic voxel
+singularity exec -e /cm/shared/containers/ANTs.sif ResampleImageBySpacing 3 $OUTPUT_DIR/aseg.nii.gz $OUTPUT_DIR/aseg_iso.nii.gz 0.4 0.4 0.4 0 0 1
+
+# Transform T2 to T1 space
+mri_vol2vol --lta $FS_MRI_DIR/transforms/T2raw.lta --mov $T2_VOL --targ $OUTPUT_DIR/aseg_iso.nii.gz --o $OUTPUT_DIR/T2toT1_iso.nii.gz
+
+# Create CSO mask in T1 native space
 TRANSFORM_PATH=$FS_MRI_DIR/transforms/synthmorph.1.0mm.1.0mm
-mri_synthmorph apply -m nearest -t uint8 $TRANSFORM_PATH/warp.to.mni152.1.0mm.1.0mm.inv.nii.gz cso_mask_mni.nii.gz $OUTPUT_DIR/cso_mask_native.nii.gz
-singularity exec -e /cm/shared/containers/ANTs.sif ResampleImageBySpacing 3 $OUTPUT_DIR/cso_mask_native.nii.gz $OUTPUT_DIR/cso_mask_native.nii.gz 0.4 0.4 0.4 0 0 1
+mri_synthmorph apply -m nearest -t uint8 $TRANSFORM_PATH/warp.to.mni152.1.0mm.1.0mm.inv.nii.gz cso_mask_mni.nii.gz $OUTPUT_DIR/cso_mask_T1.nii.gz
+mri_vol2vol --nearest --regheader --mov $OUTPUT_DIR/cso_mask_T1.nii.gz --targ $OUTPUT_DIR/aseg_iso.nii.gz --o $OUTPUT_DIR/cso_mask_iso.nii.gz
 
+# Create aseg and CSO mask in original T2 native space
+mri_vol2vol --nearest --lta-inv $FS_MRI_DIR/transforms/T2raw.lta --targ $T2_VOL --mov $OUTPUT_DIR/aseg.nii.gz --o $OUTPUT_DIR/aseg_T2.nii.gz
+mri_vol2vol --nearest --lta-inv $FS_MRI_DIR/transforms/T2raw.lta --targ $T2_VOL --mov $OUTPUT_DIR/cso_mask_T1.nii.gz --o $OUTPUT_DIR/cso_mask_T2.nii.gz
