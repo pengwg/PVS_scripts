7ad20d222122acba971668d206274d84f5bedaa7
Peng Wang <pengwg@gmail.com>
2026-02-02 13:31:33 -0500

    Add length and width stats

--- PVS_measurement_nnUNet.m
+++ PVS_measurement_nnUNet.m
@@ -6,7 +6,7 @@ data_path = {'/projects/2024-11_Perivascular_Space/PVS_B1_Analysis', ...
              '/projects/2024-11_Perivascular_Space/PVS_B2_Analysis', ...
              '/projects/2024-11_Perivascular_Space/PVS_B4_Analysis'};
 
-PVS_path = '/home/pw0032/Data/nnUNet_data/inference_pvs';
+PVS_path = '/home/pw0032/Data/nnUNet_data/inference_out_0.4/T2_SPACE';
 
 num_subjects = [140, 54, 45];
 batch_id_spec = {'PVS_%03d', 'PVS_2_%03d', 'PVS_4_%03d'};
@@ -28,28 +28,13 @@ subjectIDs = cell(total_num_subjects, 1);
 
 eTIV = zeros(total_num_subjects, 1);
 vol_brainmask = zeros(total_num_subjects, 1);
-stats = zeros(total_num_subjects, 15);
 
-vol_wm = zeros(total_num_subjects, 1);
-vol_cso = vol_wm;
-vol_frontal = vol_wm;
-vol_parietal = vol_wm;
-vol_occipital = vol_wm;
-vol_temporal = vol_wm;
-
-pvsVol_wm = zeros(total_num_subjects, 1);
-pvsVol_cso = pvsVol_wm;
-pvsVol_frontal = pvsVol_wm;
-pvsVol_parietal = pvsVol_wm;
-pvsVol_occipital = pvsVol_wm;
-pvsVol_temporal = pvsVol_wm;
-
-numPVS_wm = zeros(total_num_subjects, 1);
-numPVS_cso = numPVS_wm;
-numPVS_frontal = numPVS_wm;
-numPVS_parietal = numPVS_wm;
-numPVS_occipital = numPVS_wm;
-numPVS_temporal = numPVS_wm;
+WM = repmat(struct('Vol', 0, 'numPVS', 0, 'pvsVol', 0, 'pvsVolMean', 0, 'pvsVolMedian', 0, 'lengthMean', 0, 'lengthMedian', 0, 'widthMean', 0, 'widthMedian', 0), total_num_subjects, 1);
+CSO = WM;
+Frontal = WM;
+Parietal = WM;
+Occipital = WM;
+Temporal = WM;
 
 frontal_labels = [3003 3012 3014 3018 3019 3020 3024 3027 3028 3032 ...
                   4003 4012 4014 4018 4019 4020 4024 4027 4028 4032];
@@ -122,69 +107,35 @@ for k = 1 : length(num_subjects)
         niftiwrite(single(frontal_mask) + 2 * single(parietal_mask) + 3 * single(occipital_mask) + 4 * single(temporal_mask), ...
             [data_path_analysis '/' subject '/lobes_T2.nii.gz'], info)
         
-        vol_wm(n) = nnz(wm_mask) * prod(info.PixelDimensions);
-        vol_cso(n) = nnz(cso_mask) * prod(info.PixelDimensions);
-        vol_frontal(n) = nnz(frontal_mask) * prod(info.PixelDimensions);
-        vol_parietal(n) = nnz(parietal_mask) * prod(info.PixelDimensions);
-        vol_occipital(n) = nnz(occipital_mask) * prod(info.PixelDimensions);
-        vol_temporal(n) = nnz(temporal_mask) * prod(info.PixelDimensions);
-        
         info = niftiinfo(pvs_mask_file);
         pvs_mask_vol = niftiread(info);
-
-        pvs_wm_vol = pvs_mask_vol .* uint8(wm_mask);
-        pvs_cso_vol = pvs_mask_vol .* uint8(cso_mask);
-        pvs_frontal_vol = pvs_mask_vol .* uint8(frontal_mask);
-        pvs_parietal_vol = pvs_mask_vol .* uint8(parietal_mask);
-        pvs_occipital_vol = pvs_mask_vol .* uint8(occipital_mask);
-        pvs_temporal_vol = pvs_mask_vol .* uint8(temporal_mask);
         
-        [stats_subject, measure_all] = measurePVSstats(pvs_wm_vol, info.PixelDimensions);
-        if isempty(measure_all)
-            disp(['No PVS detected in ' subject])
-            continue
-        end
-        stats(n, :) = stats_subject';
-        numPVS_wm(n) = size(measure_all, 1);
+        [~, measure_all] = measurePVSstats(pvs_mask_vol.*uint8(wm_mask), info.PixelDimensions);
         pvsLength = measure_all(:, 1);
         pvsWidth = measure_all(:, 2);
         pvsVolume = measure_all(:, 3);
-        pvsVol_wm(n) = sum(pvsVolume);
         writetable(table(pvsLength, pvsWidth, pvsVolume), sprintf('%s/%s/PVS%d_%03d_wm_nnUNet.xlsx', data_path_analysis, subject, k, n), 'WriteMode', 'replacefile');
 
-        [pvsVol_cso(n), numPVS_cso(n)] = measurePVS(pvs_cso_vol, info.PixelDimensions);       
-        [pvsVol_frontal(n), numPVS_frontal(n)] = measurePVS(pvs_frontal_vol, info.PixelDimensions);
-        [pvsVol_parietal(n), numPVS_parietal(n)] = measurePVS(pvs_parietal_vol, info.PixelDimensions);
-        [pvsVol_occipital(n),  numPVS_occipital(n)] = measurePVS(pvs_occipital_vol, info.PixelDimensions);
-        [pvsVol_temporal(n), numPVS_temporal(n)] = measurePVS(pvs_temporal_vol, info.PixelDimensions);
-
+        WM(n) = measurePVS(pvs_mask_vol, wm_mask, info.PixelDimensions);
+        CSO(n) = measurePVS(pvs_mask_vol, cso_mask, info.PixelDimensions);       
+        Frontal(n) = measurePVS(pvs_mask_vol, frontal_mask, info.PixelDimensions);
+        Parietal(n) = measurePVS(pvs_mask_vol, parietal_mask, info.PixelDimensions);
+        Occipital(n) = measurePVS(pvs_mask_vol, occipital_mask, info.PixelDimensions);
+        Temporal(n) = measurePVS(pvs_mask_vol, temporal_mask, info.PixelDimensions);
     end
     group_i0 = group_i0 + num_subjects(k);
 end
 
-% lengthMean = stats(:, 1);
-% lengthMedian = stats(:, 2);
-% lengthStd = stats(:, 3);
-% lengthPrc25 = stats(:, 4);
-% lengthPrc75 = stats(:, 5);
-% 
-% widthMean = stats(:, 6);
-% widthMedian = stats(:, 7);
-% widthStd = stats(:, 8);
-% widthPrc25 = stats(:, 9);
-% widthPrc75 = stats(:, 10);
-% 
-% volMean = stats(:, 11);
-% volMedian = stats(:, 12);
-% volStd = stats(:, 13);
-% volPrc25 = stats(:, 14);
-% volPrc75 = stats(:, 15);
 
-T = table(subjectIDs, eTIV, vol_brainmask, vol_wm, pvsVol_wm, numPVS_wm, vol_cso, pvsVol_cso, numPVS_cso, ...
-          vol_frontal, pvsVol_frontal, numPVS_frontal, vol_parietal, pvsVol_parietal, numPVS_parietal, ...
-          vol_occipital, pvsVol_occipital, numPVS_occipital, vol_temporal, pvsVol_temporal, numPVS_temporal);
+T = table(subjectIDs, eTIV, vol_brainmask, ...
+            [WM.Vol]', [WM.numPVS]', [WM.pvsVol]', [WM.pvsVolMean]', [WM.pvsVolMedian]', [WM.lengthMean]', [WM.lengthMedian]', [WM.widthMean]', [WM.widthMedian]', ...
+            [CSO.Vol]', [CSO.numPVS]', [CSO.pvsVol]', [CSO.pvsVolMean]', [CSO.pvsVolMedian]', [CSO.lengthMean]', [CSO.lengthMedian]', [CSO.widthMean]', [CSO.widthMedian]', ...
+            [Frontal.Vol]', [Frontal.numPVS]', [Frontal.pvsVol]', [Frontal.pvsVolMean]', [Frontal.pvsVolMedian]', [Frontal.lengthMean]', [Frontal.lengthMedian]', [Frontal.widthMean]', [Frontal.widthMedian]', ...
+            [Parietal.Vol]', [Parietal.numPVS]', [Parietal.pvsVol]', [Parietal.pvsVolMean]', [Parietal.pvsVolMedian]', [Parietal.lengthMean]', [Parietal.lengthMedian]', [Parietal.widthMean]', [Parietal.widthMedian]', ...
+            [Occipital.Vol]', [Occipital.numPVS]', [Occipital.pvsVol]', [Occipital.pvsVolMean]', [Occipital.pvsVolMedian]', [Occipital.lengthMean]', [Occipital.lengthMedian]', [Occipital.widthMean]', [Occipital.widthMedian]', ...
+            [Temporal.Vol]', [Temporal.numPVS]', [Temporal.pvsVol]', [Temporal.pvsVolMean]', [Temporal.pvsVolMedian]', [Temporal.lengthMean]', [Temporal.lengthMedian]', [Temporal.widthMean]', [Temporal.widthMedian]');
 
-writetable(T, ['PVS_stats_nnUNet.xlsx'], 'WriteMode', 'replacefile')
+writetable(T, 'PVS_stats_nnUNet_Jan31.xlsx', 'WriteMode', 'replacefile')
 
 end
 
@@ -212,16 +163,27 @@ fclose(fid);
 end
 
 %%
-function [pvsVol, numPVS] = measurePVS(pvs_vol, pixelDimensions)
+function [structure_stats] = measurePVS(pvs_vol, structure_mask, pixelDimensions)
 
-[~, measure_all] = measurePVSstats(pvs_vol, pixelDimensions);
+structure_stats = struct('Vol', 0, 'numPVS', 0, 'pvsVol', 0, 'pvsVolMean', 0, 'pvsVolMedian', 0, ...
+                         'lengthMean', 0, 'lengthMedian', 0, 'widthMean', 0, 'widthMedian', 0);
+
+[stats, measure_all] = measurePVSstats(pvs_vol.*uint8(structure_mask), pixelDimensions);
 
 if ~isempty(measure_all)
-    numPVS = size(measure_all, 1);
-    pvsVol = sum(measure_all(:, 3));
-else
-    numPVS = 0;
-    pvsVol = 0;
+    structure_stats.Vol = nnz(structure_mask) * prod(pixelDimensions);
+    
+    structure_stats.numPVS = size(measure_all, 1);
+    structure_stats.pvsVol = sum(measure_all(:, 3));
+
+    structure_stats.lengthMean = stats(1);
+    structure_stats.lengthMedian = stats(2);
+
+    structure_stats.widthMean = stats(6);
+    structure_stats.widthMedian = stats(7);
+
+    structure_stats.pvsVolMean = stats(11);
+    structure_stats.pvsVolMedian = stats(12);
 end
 
 end
